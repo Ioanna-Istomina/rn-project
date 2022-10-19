c921101e0b30d4992cae69d1d88bb8bbe9a4bed6
Ioanna-Istomina <99349464+Ioanna-Istomina@users.noreply.github.com>
2022-10-19 21:52:40 +0300

    remove pathReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authReducer";
-import { pathSlice } from "./auth/pathReducer";
 
 export const store = configureStore({
   reducer: {
     [authSlice.name]: authSlice.reducer,
-    [pathSlice.name]: pathSlice.reducer,
   },
 });
